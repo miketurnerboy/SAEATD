6dfd1306a3222155a5945bc18624571ebe918eff
watashikimi <watashi.kimi@outlook.com>
2017-09-10 11:24:44 -0500

    proyecto completo

---
+++ app/Http/Controllers/ChartController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace SAEATD\http\Controllers;
+
+use Illuminate\Http\Request;
+use SAEATD\Http\Request;
+
+/*
+Controlador encargado de de hacer peticions a Mongo
+*/
+
+class ChartController extends Controller{
+
+    public function index(){
+        
+    }
+
+    public function create(){
+
+    }
+
+}
